b8da6e06741cd8b10f4ba8c50bc18840ab3cfc0c
Harsh Keshri <harshkeshri1234567@gmail.com>
2022-05-27 22:57:17 +0530

    Cart Completed

--- assests/Javascript/Shopping/ShoppingButton.js
+++ assests/Javascript/Shopping/ShoppingButton.js
@@ -4,6 +4,7 @@ class ShoppingButton{
         this.openBt = document.getElementById("shopping_cart_button");
         this.closeBt = document.getElementById("shopping_cart_close");
         this.shopCart = document.getElementById("shopping_cart");
+        this.nav = document.querySelector("nav");
         this.clicked();
     }
 
@@ -11,12 +12,14 @@ class ShoppingButton{
         this.openBt.addEventListener("click", () => {
             this.openBt.style.display = "none";
             this.closeBt.style.display = "block";
-            this.shopCart.style.display = "block";
+            this.shopCart.style.display = "flex";
+            this.nav.style.display = "none";
         });
         this.closeBt.addEventListener("click", () => {
             this.openBt.style.display = "block";
             this.closeBt.style.display = "none";
             this.shopCart.style.display = "none";
+            this.nav.style.display = "block";
         });
     }
 }
