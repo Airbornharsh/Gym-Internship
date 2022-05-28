32501a30f40644817282d4a701fea143d88e91b6
Harsh Keshri <harshkeshri1234567@gmail.com>
2022-05-28 14:53:24 +0530

    Theme Changed\

--- assests/Javascript/HeaderList.js
+++ assests/Javascript/HeaderList.js
@@ -1,17 +1,17 @@
 const HeaderList = [{
-    url: "./assests/images/headerImages/sven-mieke-jO6vBWX9h9Y-unsplash.jpg",
+    url: "./assests/images/headerImages/john-arano-h4i9G-de7Po-unsplash.jpg",
     heading: 'Workability',
     description: 'Success usually comes to those who are too busy to be looking for it.'
 },{
-    url: "./assests/images/headerImages/anastase-maragos-9dzWZQWZMdE-unsplash.jpg",
+    url: "./assests/images/headerImages/jonathan-borba-zfPOelmDc-M-unsplash.jpg",
     heading: 'Dedication',
     description: 'Bodybuilding is much like any other sport. To be successful, you must dedicate yourself 100% to your training, diet and mental approach.'
 },{
-    url: "./assests/images/headerImages/daniel-apodaca-WdoQio6HPVA-unsplash.jpg",
+    url: "./assests/images/headerImages/karsten-winegeart-0Wra5YYVQJE-unsplash.jpg",
     heading: 'Toughness',
     description: 'Tough times don’t last. Tough people do.'
 },{
-    url: "./assests/images/headerImages/risen-wang-20jX9b35r_M-unsplash.jpg",
+    url: "./assests/images/headerImages/anastase-maragos-7kEpUPB8vNk-unsplash.jpg",
     heading: 'Discipline',
     description: 'There is a moment when you get older when your metabolism slows down and you don’t feel like working out any more, so you don’t want to keep yourself fit any more, but that’s your decision. Why should you be judged for it?'
 }];
